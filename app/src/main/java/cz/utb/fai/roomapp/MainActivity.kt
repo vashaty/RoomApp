f68ab05a3a0ed3c528632c239d978d6fd957ac78
vashaty <v_toncer@utb.cz>
2021-11-27 17:10:38 +0100

    recycler view initialized

--- app/src/main/java/cz/utb/fai/roomapp/MainActivity.kt
+++ app/src/main/java/cz/utb/fai/roomapp/MainActivity.kt
@@ -2,10 +2,17 @@ package cz.utb.fai.roomapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import cz.utb.fai.roomapp.db.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
