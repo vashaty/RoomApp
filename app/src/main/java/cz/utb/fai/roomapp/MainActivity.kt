ad0c8bbddd9faa982c122aaebfeb8a76f2fec0ac
vashaty <v_toncer@utb.cz>
2021-11-27 18:15:39 +0100

    newWord activity added

--- app/src/main/java/cz/utb/fai/roomapp/MainActivity.kt
+++ app/src/main/java/cz/utb/fai/roomapp/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
